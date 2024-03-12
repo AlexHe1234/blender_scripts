bd2a3344bf1e8db7f95f0b14726b158e2716af8d
alexhe <alexhe12345678@gmail.com>
2024-03-12 12:03:34 +0800

    camera extraction

--- extract_camera_animation.py
+++ extract_camera_animation.py
@@ -3,8 +3,25 @@ import numpy as np
 from mathutils import Matrix, Vector
 
 
+def r2blender(r: np.ndarray):
+    adjustment_mat = np.array([[1.,  0.,  0.],
+                               [0., -1.,  0.], 
+                               [0.,  0., -1.]])
+    blender_camera_rotation = Matrix(r.T @ adjustment_mat).to_euler()
+    return blender_camera_rotation
+
+
+def blender_euler2opencv_r(euler):
+    mat_raw = np.asarray(euler.to_matrix())
+    adjustment_mat = np.array([[1.,  0.,  0.],
+                               [0., -1.,  0.], 
+                               [0.,  0., -1.]])
+    r = (mat_raw @ adjustment_mat).T
+    return r
+
+
 # return opencv k, r, t matrices
-def extract_camera_param(camera_name: str):
+def extract_camera_param():
     scene = bpy.context.scene
     camera = scene.camera
     
@@ -20,23 +37,25 @@ def extract_camera_param(camera_name: str):
     k[1, 2] = H / 2.
     
     # r
-    
+    cam_euler = camera.rotation_euler
+    r = blender_euler2opencv_r(cam_euler)
     
     # t
     cam_pose = camera.location
+    t = -r @ cam_pose
     
     return k, r, t
 
 
 def extract_camera_animation(frame_start: int,  # inclusive
                              frame_end: int,  # inclusive
-                             camera_name: str):
+                             ):
     ks = []
     rs = []
     ts = []
     for f in range(frame_start, frame_end + 1):
         bpy.context.scene.frame_set(f)
-        k, r, t = extract_camera_param(camera_name=camera_name)
+        k, r, t = extract_camera_param()
         ks.append(k)
         rs.append(r)
         ts.append(t)
@@ -46,3 +65,8 @@ def extract_camera_animation(frame_start: int,  # inclusive
     ts = np.stack(ts)  # F, 3
     
     return ks, rs, ts
+
+
+if __name__ == '__main__':
+    ks, rs, ts = extract_camera_animation(1, 201)
+    np.save('/Users/alexhe/Downloads/camera_animation.npy', {'k': ks, 'r': rs, 't': ts})
